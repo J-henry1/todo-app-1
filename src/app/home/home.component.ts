b8e24093d1a08c2de167b2227384b86cf8f878e0
JARED HENRY <109435235+J-henry1@users.noreply.github.com>
2024-04-12 14:41:29 -0400

    able to login now and get auth code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,19 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class HomeComponent {
 
+  constructor(private loginUser: TodoService) {
+    
+  }
+
   emailFormControl = new FormControl('test@test.com', [Validators.required, Validators.email]);
   passwordFormControl = new FormControl('test', [Validators.required, Validators.minLength(4)]);
+
+  LoginUser(){
+    if(!this.emailFormControl.invalid && !this.passwordFormControl.invalid){
+      this.loginUser.LoginUser(this.emailFormControl.value as string, this.passwordFormControl.value as string);
+    }
+    else{
+      console.log("Failed to login user");
+    }
+  }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 
@@ -31,5 +31,23 @@ export class TodoService {
     
   }//end Register User method
 
-  
-}
+  async LoginUser(email: string, password: string){
+    let userData = {
+      email: email,
+      password: password,
+    };
+
+    try{
+      const headers = new HttpHeaders({
+        'Authorization': 'Basic ' + btoa(`${email}:${password}`)//used stack overflow for this - https://stackoverflow.com/questions/53613529/angular-6-http-get-request-with-http-basic-authentication
+      });
+
+      let authToken = await firstValueFrom(this.httpClient.post('https://unfwfspring2024.azurewebsites.net/user/login', userData, { headers: headers }));
+      console.log(authToken);
+      return true;
+    } catch(error) {
+      console.log(error);
+      return false;
+    }
+  }
+}//end TodoService Class
