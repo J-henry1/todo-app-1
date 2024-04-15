02c78c113eb5d602726442798172403c9801d326
JARED HENRY <109435235+J-henry1@users.noreply.github.com>
2024-04-15 13:50:41 -0400

    added function for added create todo

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,10 @@ export class TodoService {
   }
 
   UserLoggedIn:EventEmitter<string>= new EventEmitter<string>();
+
+  currentUserToken: null=null;
+
+
 
   async RegisterUser(email: string, password: string, name:string){
 
@@ -46,20 +50,37 @@ export class TodoService {
       });
 
       let authToken = await firstValueFrom(this.httpClient.post('https://unfwfspring2024.azurewebsites.net/user/login', userData, { headers: headers }));
-      localStorage.setItem('currentUserToken',JSON.stringify(userData));
+      localStorage.setItem('authToken',JSON.stringify(userData));
       this.UserLoggedIn.emit(email);
       console.log(authToken);
       this.router.navigate(['/Todos']);
-      return true;
+      return firstValueFrom(of(null));
     } catch(error) {
       console.log(error);
       return false;
     }
   }
 
-  // async GetTodos(){
+  async CreateTodo(title: string, publicAccess: boolean){
+    
+    let todoData = {
+      title: title,
+      public: publicAccess,
+    }
 
-  //   let user = localStorage.getItem()
+    const token = localStorage.getItem('currentUserToken');
 
-  // }
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    try {
+      const response = await this.httpClient.post('https://unfwfspring2024.azurewebsites.net/todo', todoData, { headers });
+      console.log(response); // Log the response from the server
+      return response; // Return the response if needed
+    } catch(error) {
+      console.error(error); // Log any errors that occur
+      throw error; // Rethrow the error to handle it in the calling code
+    }
+  }
 }//end TodoService Class
--- src/app/todo-lists/todo-lists.component.ts
+++ src/app/todo-lists/todo-lists.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-lists',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './todo-lists.component.scss'
 })
 export class TodoListsComponent {
+
+  constructor(private CreateTodo: TodoService) {
+    
+  }
+
+  title = new FormControl('', [Validators.required])
+  publicAccess = new FormControl('', [Validators.required]);
+
+  CreateTodos(){
+    
+    if(this.title.valid && this.publicAccess.valid){
+      this.CreateTodo.CreateTodo(this.title.value as string, this.publicAccess.value as unknown as boolean);
+    }
+    else{
+      console.log("Failed to create todo");
+    }
+  }
 
 }
